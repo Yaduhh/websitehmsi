b153dce7d310408758559155fb44b92de6a5c236
aryobd <aryobd@gmail.com>
2023-09-12 13:33:14 +0700

    feat: use userId for article submission

--- app/api/artikel/route.js
+++ app/api/artikel/route.js
@@ -7,13 +7,18 @@ const prisma = new PrismaClient();
 export async function POST(req, res) {
   if (req.method == "POST") {
     const data = await req.json();
-    const { title, content } = data;
+    const { title, content, session } = data;
+
+    console.log(`session`, session);
+
+    const userId = session.user.id;
 
     try {
       const response = await prisma.article.create({
         data: {
           title: title,
           content: content,
+          authorId: userId,
         },
       });
       return NextResponse.json({ data: response })
